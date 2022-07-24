edfba359919e41b17a20a3ca13a1c92c5e59a0fd
Ralph Zitz <ralph@zitz.dk>
2022-07-24 09:20:57 +0200

    Remove unneeded break statements

--- d/silvere/rooms/west/westroad8.c
+++ d/silvere/rooms/west/westroad8.c
@@ -22,19 +22,15 @@ string CheckNoise()
     case ND_SUNRISE: return // 3
 		       "You hear the sounds of shutters opening. Somewhere, "
 		       "a bird sings.\n";
-    break;
     case ND_SUNSET: return // 9
 		      "Slowly the noise around you dims a bit as the street "
 		      "gets emptier.\n";
-    break;
     case ND_EARLY_MORNING..ND_LATE_AFTERNOON: return // 4..8
 		   "The noise is awful. Everybody seems to be shouting at "
 		   "the top of their lungs. You feel slightly dizzy.\n";
-    break;
     case ND_NIGHT..ND_DAWN: // 0..2
     case ND_TWILIGHT..ND_EVENING: return // 10..11
 		     "Everything is silent.\n";
-    break;
     }
     return 0;
 }
@@ -47,11 +43,9 @@ string CheckPeople()
     case ND_SUNRISE..ND_SUNSET: return
 	 "Brightly dressed people move about on their business everywhere "
 	 "around you.\n";
-    break;
     default: return "It is night, and nobody is on the street.\n";
       break;
     }
-    return "It is night, and nobody is on the street.\n";
 }
 
 string CheckVines()
@@ -60,17 +54,13 @@ string CheckVines()
    {
       case WINTER: return
          "The vines are brown and leafless.\n";
-         break;
       case SPRING: return
          "The first tender green leaves are showing on the vines.\n";
-         break;
       case SUMMER: return
          "Dark green leaves cover the vines.\n";
-         break;
       case AUTUMN: return
          "The leaves seem to glow in the colours of autumn: yellow, "
 	 "golden orange and deep red. They look very pretty.\n";
-         break;
    }
    return 0;
 }

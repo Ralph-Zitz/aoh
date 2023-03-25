5c89da139f91b6073c4d6c1e9f426922a06f0e9e
Ralph Zitz <ralph@zitz.dk>
2023-03-25 16:31:41 +0100

    Move TLS block out

--- std/living/moving.c
+++ std/living/moving.c
@@ -194,12 +194,12 @@ public varargs int move(mixed dest, int method, mixed extra) {
 
 public varargs int remove () {
   if (interactive()) {
-      tell_object(THIS, "Good bye! Come back soon!\n");
-#ifdef __TLS__
-      if (tls_available())
-          tls_deinit_connection(THIS);
-#endif
+    tell_object(THIS, "Good bye! Come back soon!\n");
   }
+#ifdef __TLS__
+  if (tls_available())
+    tls_deinit_connection(THIS);
+#endif
   destruct (THIS);
   return 1;
 }

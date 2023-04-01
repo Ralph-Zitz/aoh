cef19f9fe9670b6b93425a3bf38aff393fc72309
Ralph Zitz <Ralph.Zitz@LEGO.com>
2023-04-01 08:57:16 +0200

    Clean up header file

--- d/sykorath/sys/path.h
+++ d/sykorath/sys/path.h
@@ -14,32 +14,32 @@
 
 /* start with the main path for easy moving of the files */
 #define BASE                 "/d/sykorath/"
-#define STD(x)               BASE+"std/"+x
-#define ROOM                 BASE+"rooms/"
-#define QUEST(x)             BASE+"quests/"+x
-#define OBJ(x)               BASE+"obj/"+x
-#define NPC(x)               BASE+"npc/"+x
-#define LOG(x)               BASE+"log/"+x
+#define STD(x)               BASE "std/" x
+#define ROOM                 BASE "rooms/"
+#define QUEST(x)             BASE "quests/" x
+#define OBJ(x)               BASE "obj/" x
+#define NPC(x)               BASE "npc/" x
+#define LOG(x)               BASE "log/" x
 
-#define TXT(x)               BASE+"doc/player/"+x
+#define TXT(x)               BASE "doc/player/" x
 
 /* areas */
-#define DFOREST(x)           ROOM+"dforest/"+x
+#define DFOREST(x)           ROOM "dforest/" x
 
-#define INNER(x)             ROOM+"inner/"+x
-#define OUTER(x)             ROOM+"outer/"+x
-#define SEAROOMS(x)          ROOM+"sea/"+x
-#define TUNNELS(x)           ROOM+"tunnels/"+x
-#define NEWBIE(x)            ROOM+"newbiepark/"+x
-#define UNDERGROUND(x)       ROOM+"underground/"+x
-#define TOWNWALLI(x)         INNER("townwall/")+x
-#define TOWNWALLO(x)         OUTER("townwall/")+x
+#define INNER(x)             ROOM "inner/" x
+#define OUTER(x)             ROOM "outer/" x
+#define SEAROOMS(x)          ROOM "sea/" x
+#define TUNNELS(x)           ROOM "tunnels/" x
+#define NEWBIE(x)            ROOM "newbiepark/" x
+#define UNDERGROUND(x)       ROOM "underground/" x
+#define TOWNWALLI(x)         INNER("townwall/" x)
+#define TOWNWALLO(x)         OUTER("townwall/" x)
 
 /*
    old define - remove it if all rooms moved to inner and outer
    city directories
 */
-#define SYKORATH(x)          ROOM+x
+#define SYKORATH(x)          ROOM x
 
 /* some useful defines */
 #define BASEROOM             STD("room")
